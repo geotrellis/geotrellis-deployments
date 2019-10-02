30a9149e8de737c6cc65a1de6c7ab6c526f884fd
jpolchlo <jpolchlopek@azavea.com>
2019-10-02 14:42:20 -0400

    Clean up

--- rpms/fetch.sh
+++ rpms/fetch.sh
@@ -5,10 +5,8 @@ then
     URI=$(echo $1 | sed 's,/$,,')
     make src
     aws s3 sync $URI rpmbuild/RPMS/x86_64/
-    mv -f rpmbuild/RPMS/x86_64/*.whl wheel/
     touch rpmbuild/RPMS/x86_64/*.rpm
     touch rpmbuild/RPMS/x86_64/hdf5-*.rpm
     touch rpmbuild/RPMS/x86_64/netcdf-*.rpm
     touch rpmbuild/RPMS/x86_64/gdal241-*.rpm
-    touch rpmbuild/RPMS/x86_64/jupyterhub-*.rpm
 fi
