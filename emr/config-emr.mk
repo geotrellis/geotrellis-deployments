497c3d1abf73c2ca21789c1071fc90470ab1f41e
jpolchlo <jpolchlo@users.noreply.github.com>
2020-03-03 17:37:04 -0500

    Delete config-emr.mk

--- emr/config-emr.mk
+++
@@ -1,8 +0,0 @@
-export NAME := Geotrellis cluster - ${USER}
-export SECURITY_GROUP :=
-export MASTER_INSTANCE := m3.xlarge
-export MASTER_PRICE := 0.50
-export WORKER_INSTANCE := m3.xlarge
-export WORKER_PRICE := 0.10
-export WORKER_COUNT := 1
-export USE_SPOT := true
